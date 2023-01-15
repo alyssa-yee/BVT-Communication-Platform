dd15aea2b3a2cb901d1ea51b3918bb66c5a4828b
alyssa-yee <alyssayee99@gmail.com>
2023-01-15 04:56:49 -0800

    3 - navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,27 +4,27 @@ export default function Navbar(){
     return(
         <div class = "navdiv">
             <nav>
-                <img src = {require ('../images/DDlogo.png')} className = "nav--DD" alt ="DD logo"/>
+                <img src = {require ('../images/whiteDDlogo.png')} className = "nav--DD" alt ="DD logo"/>
                 <ul className='list'>
-                    <li><a href = "http://google.com/" >
-                    <img src = {require ('../images/circle.png')} className = "servers" alt ="Server 1"/>    
-                    </a></li>
-                    <li><a href = "http://google.com/" >
-                    <img src = {require ('../images/circle.png')} className = "servers" alt ="Server 2"/>    
-                    </a></li>
-                    <li><a href = "http://google.com/" >
-                    <img src = {require ('../images/circle.png')} className = "servers" alt ="Server 3"/>    
-                    </a></li>
-                    <li><a href = "http://google.com/" >
-                    <img src = {require ('../images/circle.png')} className = "servers" alt ="Server 4"/>    
-                    </a></li>
-                    <li><a href = "http://google.com/" >
-                    <img src = {require ('../images/circle.png')} className = "servers" alt ="Server 5"/>    
-                    </a></li>
+                    <li>
+                        <a href = "http://google.com/"><button className='servers'>S1</button></a>
+                    </li>
+                    <li>
+                        <a href = "http://google.com/"><button className='servers'>S1</button></a>
+                    </li>
+                    <li>
+                        <a href = "http://google.com/"><button className='servers'>S1</button></a>
+                    </li>
+                    <li>
+                        <a href = "http://google.com/"><button className='servers'>S1</button></a>
+                    </li>
+                    <li>
+                        <a href = "http://google.com/"><button className='servers'>S1</button></a>
+                    </li>
 
-                    <li classmate  = 'right-links'><a href = "http://google.com/"> Help </a></li>
-                    <li classmate  = 'right-links'><a href = "http://google.com/"> Settings </a></li>
-                    <li classmate  = 'right-links'><button> Log Out </button></li>
+                    <li className  = 'help-settings'><a href = "http://google.com/"> Help </a></li>
+                    <li className = 'help-settings'><a href = "http://google.com/"> Settings </a></li>
+                    <li><button className = 'logout'> Log Out </button></li>
                     
                 </ul>
             </nav>
